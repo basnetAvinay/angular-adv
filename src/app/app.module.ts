c9668b855f8c6051709366205755c424109fdee4
Avinay Basnet <avinay.basnet@cotiviti.com>
2022-12-13 12:02:59 +0545

    Day 2: Todo UI Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TodoModule} from './forms/todo/todo.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
